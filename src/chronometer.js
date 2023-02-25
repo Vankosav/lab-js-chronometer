cb01662b68586a2d37a3b713cb4b76909888e410
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-02-25 15:39:50 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
+
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0, 
+    this.intervalId = null;
   }
+
+  /*start(printTimeCallback) {
+    
+      console.log(currentTime);
+      currentTime++;*/
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval( ()=> {
+      // Call printTimeCallback every 1000 milliseconds
+      if (printTimeCallback) printTimeCallback();
+      this.currentTime++;
+      console.log(this.currentTime)
+    }, 1000);
+    // Handle case where current time is negative
   }
 
   getMinutes() {
-    // ... your code goes here
+      return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (("0" + value).slice(-2));
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const mm = this.computeTwoDigitNumber(this.getMinutes());
+    const ss = this.computeTwoDigitNumber(this.getSeconds());
+    return `${mm}:${ss}`
   }
 }
